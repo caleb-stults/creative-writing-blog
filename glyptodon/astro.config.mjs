3eca9020ad9a804acba242dfcf7e4cc00ecca141
caleb-stults <cstults92@gmail.com>
2026-06-20 22:58:28 -0600

    revamping keystatic configs

--- glyptodon/astro.config.mjs
+++ glyptodon/astro.config.mjs
@@ -3,37 +3,22 @@ import { CONFIG } from "./src/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import playformCompress from "@playform/compress";
-
-import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   base: "/",
   site: 'https://blog.hoss-movies.com',
-  adapter: undefined,
   output: 'static',
   integrations: [
     sitemap(),
     mdx(),
     tailwind(),
     playformCompress(),
-    keystatic(),
   ],
   markdown: {
     shikiConfig: {
       theme: "material-theme-darker",
       langs: [],
-    },
-  },
-  content: {
-    collections: {
-      posts: {
-        schema: "src/content/config.ts#posts",
-      },
-      finds: {
-        schema: "src/content/config.ts#finds",
-      },
     },
   },
   vite: {
