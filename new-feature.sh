3fc15113301bbcb1f15d73d1adefdf98f149d242
caleb-stults <cstults92@gmail.com>
2026-06-24 21:38:20 -0600

    Manual Update: 2026-06-24 21:38:19

---
+++ new-feature.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FEATURE_NAME=$1
+SECTION=$2
+FILENAME=$3
+TITLE=$4
+
+git checkout main
+git pull origin main
+
+git checkout -b "$FEATURE_NAME"
+
+./new-post.sh "$SECTION" "$FILENAME" "$TITLE"
+
+echo "Branch '$FEATURE_NAME' created and post initialized."
